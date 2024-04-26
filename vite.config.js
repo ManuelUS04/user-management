4bf2f33bd69e9658c430120be8702f584036b289
ManuelUS04 <jmanuelus24@gmail.com>
2024-04-25 23:23:49 -0500

    vitejs

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,14 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: ["resources/js/app.js"],
             refresh: true,
         }),
-        livewire({ 
-            refresh: ['resources/css/app.css'],
-          }),
+        livewire({
+            refresh: ["resources/css/app.css"],
+        }),
     ],
 });
