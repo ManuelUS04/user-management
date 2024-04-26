cb6c8fba28f3721ff9494fcc95079a114d058fac
ManuelUS04 <jmanuelus24@gmail.com>
2024-04-26 11:10:44 -0500

    build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import fs from 'fs';
 
 export default defineConfig({
     plugins: [
@@ -11,14 +9,6 @@ export default defineConfig({
                 'resources/js/app.js',
             ],
             refresh: true,
-            assetUrl: (url) => url.startsWith('/') ? `https://user-management-production-d279.up.railway.app${url}` : url,
         }),
     ],
-    server: {
-        https: {
-          key: fs.readFileSync('./server.key'),
-          cert: fs.readFileSync('./server.cert')
-        },
-        http2: false // Deshabilita HTTP/2 para que funcione en entornos locales con HTTP
-    },
 });
