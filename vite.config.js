bc83b4c5c3c3a6101a3899c51637ccde1f734632
ManuelUS04 <jmanuelus24@gmail.com>
2024-04-26 10:54:49 -0500

    build

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
@@ -9,6 +10,13 @@ export default defineConfig({
                 'resources/js/app.js',
             ],
             refresh: true,
+            assetUrl: (url) => url.startsWith('/') ? `https://user-management-production-d279.up.railway.app${url}` : url,
         }),
     ],
+    server: {
+        https: {
+          key: require('fs').readFileSync('./server.key'),
+          cert: require('fs').readFileSync('./server.cert')
+        },
+    },
 });
