0c9bd5d9e92931b0870e575b1a0a55c32b010c1c
ManuelUS04 <jmanuelus24@gmail.com>
2024-04-26 00:21:26 -0500

    vitejs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,12 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <!-- @vite(['resources/css/app.css', 'resources/js/app.js']) -->
+        <!-- Styles -->
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
+        <!-- Scripts -->
+        <script src="{{ mix('js/app.js') }}" defer></script>
 
         <!-- Styles -->
         @livewireStyles
